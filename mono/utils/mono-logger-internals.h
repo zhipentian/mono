d6358d1ac25091ad21de3d953d0a5ec8679cdfe7
Aleksey Kliger <alklig@microsoft.com>
2020-03-19 11:58:56 -0400

    [utils] add MONO_TRACE_METADATA_UPDATE
    Use MONO_LOG_MASK=metadata-update

--- mono/utils/mono-logger-internals.h
+++ mono/utils/mono-logger-internals.h
@@ -29,6 +29,7 @@ typedef enum {
 	MONO_TRACE_TAILCALL           = 1 << 16,
 	MONO_TRACE_PROFILER           = 1 << 17,
 	MONO_TRACE_TIERED             = 1 << 18,
+	MONO_TRACE_METADATA_UPDATE    = 1 << 19,
 } MonoTraceMask;
 
 MONO_API_DATA GLogLevelFlags mono_internal_current_level;
